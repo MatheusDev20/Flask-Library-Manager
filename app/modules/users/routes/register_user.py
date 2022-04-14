051035aa9fb98e066ea42a7b99a4e546fc779b5d
Matheus <matheusdev20@gmail.com>
2022-04-14 04:49:10 -0300

    feat: first insert on database by users screen

--- app/modules/users/routes/register_user.py
+++ app/modules/users/routes/register_user.py
@@ -1,6 +1,9 @@
 from app.modules.users import users_bp
 from flask import request, render_template
 from app.modules.users.user_form import UserRegistrationForm
+from app.db.models import User
+from flask import flash
+from app.db import db
 
 users = [
     {
@@ -16,9 +19,18 @@ users = [
 @users_bp.route('/register', methods=['POST', 'GET'])
 def register_user():
     form = UserRegistrationForm()
-    if form.validate_on_submit():
-        print(form.errors)
-        print(form.data)
 
-    print(form.errors)
+    if form.validate_on_submit():
+        user_data = form.data
+        new_user = {
+            'username': form.data['username'],
+            'email': form.data['email'],
+            'password': form.data['password'],
+            'admin': form.data['admin'],
+            'role': form.data['role'].upper(),
+            'avatar': 'RandomS3String'
+        }
+        db.session.add(User(**new_user))
+        db.session.commit()
+
     return render_template('register_user.html', title='Register User', form=form)
